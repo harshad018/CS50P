6e41b3ffc0c42c90281ef1403d755d909a47d8d4
Harshad Ghutukade <108193335+harshad018@users.noreply.github.com>
2024-07-26 10:08:56 +0530

    Update adieu.py

--- Labs/Week4/adieu.py
+++ Labs/Week4/adieu.py
@@ -1,1 +1,23 @@
+names = []
+names2 = []
+while True:
+    try:
+        userinput = input("Input: ")
 
+        names.append(userinput)
+
+        
+    except:
+        print()
+        break
+
+
+for name in range(len(names)-1):
+    names2.append(names[name])
+
+
+
+if len(names2) == 0:
+    print("Adieu, adieu, to ", ", ".join(names))
+else:
+    print("Adieu, adieu, to ", ", ".join(names2) , "and", names[len(names)-1])
